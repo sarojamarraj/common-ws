538c1d9acf7b3d4ebea9df9abfd73bd15fb41780
rnarine <rnarine@d15b92d0-1eae-4802-a2b4-c31dc5a715d9>
2018-09-24 17:00:11 +0000

    Add ComponentScan so that it can be picked up on startup
    git-svn-id: http://104.153.56.169:18080/svn/common-ws@22 d15b92d0-1eae-4802-a2b4-c31dc5a715d9

---
+++ dao/trunk/src/main/java/com/freightcom/common/ws/dao/DaoConfig.java
@@ -0,0 +1,7 @@
+package com.freightcom.common.ws.dao;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan({ "com.freightcom.common.ws.dao" })
+public class DaoConfig {
+}
