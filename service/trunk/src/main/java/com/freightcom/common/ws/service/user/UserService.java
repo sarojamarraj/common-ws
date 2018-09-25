795afbc26d414dd08ce54a94afbaf0d22373b07f
rnarine <rnarine@d15b92d0-1eae-4802-a2b4-c31dc5a715d9>
2018-09-25 20:22:52 +0000

    Moved the Services to the infrastructure package
    git-svn-id: http://104.153.56.169:18080/svn/common-ws@32 d15b92d0-1eae-4802-a2b4-c31dc5a715d9

--- service/trunk/src/main/java/com/freightcom/common/ws/service/user/UserService.java
+++
@@ -1,6 +0,0 @@
-package com.freightcom.common.ws.service.user;
-
-public interface UserService {
-	public void sendValidationEmail(String userName);
-	public void confirmValidationEmail(String token);
-}
--- service/trunk/src/main/java/com/freightcom/common/ws/service/user/impl/ValidationException.java
+++
@@ -1,9 +0,0 @@
-package com.freightcom.common.ws.service.user.impl;
-
-public class ValidationException extends RuntimeException {
-	private static final long serialVersionUID = -6950863177244518733L;
-
-	public ValidationException(String message) {
-		super(message);
-	}
-}
