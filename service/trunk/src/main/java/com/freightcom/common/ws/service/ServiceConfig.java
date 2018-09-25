2f175c7463b55bd215c845b2dcaef8a725df6e0a
rnarine <rnarine@d15b92d0-1eae-4802-a2b4-c31dc5a715d9>
2018-09-25 20:21:31 +0000

    Moved the Services to the infrastructure package
    git-svn-id: http://104.153.56.169:18080/svn/common-ws@31 d15b92d0-1eae-4802-a2b4-c31dc5a715d9

--- service/trunk/src/main/java/com/freightcom/common/ws/service/ServiceConfig.java
+++ service/trunk/src/main/java/com/freightcom/common/ws/service/ServiceConfig.java
@@ -11,40 +11,15 @@ import org.springframework.context.annotation.PropertySources;
 import org.springframework.jms.connection.CachingConnectionFactory;
 import org.springframework.jms.core.JmsTemplate;
 
-import com.freightcom.common.ws.service.mail.impl.AddToListRule;
-import com.freightcom.common.ws.service.mail.impl.SingleEmailRule;
-import com.github.alexanderwe.bananaj.connection.MailChimpConnection;
-import com.microtripit.mandrillapp.lutung.MandrillApi;
-
 @PropertySources({
-	@PropertySource(value="classpath:mailchimp.properties"),
-	@PropertySource(value="classpath:mailchimp-overrides.properties",ignoreResourceNotFound=true),
 	@PropertySource(value="classpath:queue.properties"),
 	@PropertySource(value="classpath:queue-overrides.properties",ignoreResourceNotFound=true)
 })
 @Configuration
 @ComponentScan({ "com.freightcom.common.ws.service" })
 public class ServiceConfig {
-	@Value("${mailchimp.mandrill.apikey}")
-	private String mandrillApiKey;
-	
-	@Value("${mailchimp.apikey}")
-	private String mailChimpApiKey;
-	
 	@Value("${activemq.broker-url}")
 	private String brokerUrl;
-	
-	@Bean
-	public MandrillApi mandrillApi() {
-		MandrillApi mandrillApi = new MandrillApi(mandrillApiKey);
-		return mandrillApi;
-	}
-	
-	@Bean
-	public MailChimpConnection mailChimpConnection() {
-		MailChimpConnection mailChimpConnection = new MailChimpConnection(mailChimpApiKey);
-		return mailChimpConnection;
-	}
 	
 	@Bean
 	public ActiveMQConnectionFactory activeMQConnectionFactory() {
@@ -62,15 +37,5 @@ public class ServiceConfig {
 	@Bean
 	public JmsTemplate jmsTemplate() {
 		return new JmsTemplate(cachingConnectionFactory());
-	}
-	
-	@Bean
-	public Rules mailRequestRules() {
-		SingleEmailRule singleEmailRule = new SingleEmailRule();
-		AddToListRule addToListRule = new AddToListRule();
-		Rules rules = new Rules();
-        rules.register(singleEmailRule);
-        rules.register(addToListRule);
-        return rules;
 	}
 }
